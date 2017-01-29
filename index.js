e1f9cc93d53284236744617652534837719e5105
TamirL <tamir168@gmail.com>
2017-01-29 18:45:00 +0200

    chore(module-starter): remove leftover files

--- index.js
+++
@@ -1,7 +0,0 @@
-var israelInternationalPrefix = "+972";
-
-module.exports = function(phoneNumber) {
-    var cleanNumber = phoneNumber.replace(/[^0-9+]/g, "");
-    var localNumber = cleanNumber.startsWith(israelInternationalPrefix) ? "0" + cleanNumber.substring(4) : cleanNumber;
-    return localNumber.replace("+", "");
-};
